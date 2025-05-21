e351abb8749118c6196170162a4cbe31bf94e42a
sruthidesai46 <desaisruthi2001@gmail.com>
2025-05-21 16:55:09 +0530

    Add files via upload

---
+++ java/hello.java
@@ -0,0 +1,14 @@
+
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+            //System.out.println("hello world");
+            
+             String name = "sruthi";
+            System.out.println(name);
+            name="chinni";
+            System.out.println(name);
+	}
+
+}
